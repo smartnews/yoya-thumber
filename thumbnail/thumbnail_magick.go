6165e1209296f6bda7e4b6f919b6d4bd9265bfa8
Yoshihiro Yamazaki <yoya@awm.jp>
2020-06-10 03:29:24 +0900

    err nil checking of SetImageCompressionQuality.

--- thumbnail/thumbnail_magick.go
+++ thumbnail/thumbnail_magick.go
@@ -804,8 +804,13 @@ func MakeThumbnailMagick(src io.Reader, dst http.ResponseWriter, params Thumbnai
 			return err
 		}
 	}
-	err = mw.SetImageCompressionQuality(uint(params.Quality)) // JPEG, WebP
-	err = mw.SetCompressionQuality(uint(params.Quality))      // HEIC
+	// JPEG, WebP
+	err = mw.SetImageCompressionQuality(uint(params.Quality))
+	if err != nil {
+		panic(err)
+	}
+	// HEIC
+	err = mw.SetCompressionQuality(uint(params.Quality))
 	if err != nil {
 		panic(err)
 	}
